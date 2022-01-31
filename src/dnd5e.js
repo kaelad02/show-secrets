1cbd6661f6a827820123f4b4ee0627a779573914
Chris Seieroe <kaelad02@users.noreply.github.com>
2022-01-31 05:11:14 -0800

    refactor to make it easier to add more systems

---
+++ src/dnd5e.js
@@ -0,0 +1,15 @@
+/**
+ * Specific initialization for the dnd5e system. Specifically, wraps
+ * Item5e.getChatData so it keeps secrets when creating the chat message.
+ */
+export default function () {
+  libWrapper.register(
+    "show-secrets",
+    "CONFIG.Item.documentClass.prototype.getChatData",
+    function (wrapped, htmlOptions = {}) {
+      htmlOptions.secrets = this.actor.isOwner;
+      return wrapped(htmlOptions);
+    },
+    "WRAPPER"
+  );
+}
--- src/module.js
+++ src/module.js
@@ -1,40 +1,37 @@
+import dnd5e from "./dnd5e.js";
+
 Hooks.once("init", () => {
-  // override core Foundry method so ChatMessage doesn't strip out secrets
+  // initialize based on the game system
+  switch (game.system.id) {
+    case "dnd5e":
+      initCommon();
+      dnd5e();
+      break;
+  }
+});
+
+/**
+ * Common initialization to change core Foundry code. Specifically, overrides
+ * ChatMessage.prepareData so it doesn't strip out secrets.
+ */
+function initCommon() {
   libWrapper.register(
     "show-secrets",
     "CONFIG.ChatMessage.documentClass.prototype.prepareData",
-    onPrepareData,
+    function () {
+      Object.getPrototypeOf(ChatMessage).prototype.prepareData.apply(this);
+      const actor =
+        this.constructor.getSpeakerActor(this.data.speaker) ||
+        this.user?.character;
+      const rollData = actor ? actor.getRollData() : {};
+      const owner = actor ? actor.isOwner : false;
+      this.data.update({
+        content: TextEditor.enrichHTML(this.data.content, {
+          secrets: owner,
+          rollData,
+        }),
+      });
+    },
     "OVERRIDE"
   );
-
-  // wrap Item5e so it keeps secrets when creating the chat message
-  Hooks.once("init", () => {
-    libWrapper.register(
-      "adv-reminder",
-      "CONFIG.Item.documentClass.prototype.getChatData",
-      onGetChatData,
-      "WRAPPER"
-    );
-  });
-});
-
-function onPrepareData() {
-  Object.getPrototypeOf(ChatMessage).prototype.prepareData.apply(this);
-
-  const actor =
-    this.constructor.getSpeakerActor(this.data.speaker) || this.user?.character;
-  const rollData = actor ? actor.getRollData() : {};
-  const owner = actor ? actor.isOwner : false;
-  this.data.update({
-    content: TextEditor.enrichHTML(this.data.content, {
-      secrets: owner,
-      rollData,
-    }),
-  });
-}
-
-function onGetChatData(wrapped, htmlOptions = {}) {
-  htmlOptions.secrets = this.actor.isOwner;
-  const result = wrapped(htmlOptions);
-  return result;
 }
