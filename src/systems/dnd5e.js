a0d384a18d812d8715f3e166ab3bae6b5e89fe9e
Chris Seieroe <kaelad02@users.noreply.github.com>
2024-02-03 13:03:08 -0800

    Add check to register only one wrapper

--- src/systems/dnd5e.js
+++ src/systems/dnd5e.js
@@ -5,25 +5,27 @@ import { showSecrets } from "../settings.js";
  * Item5e.getChatData so it keeps secrets when creating the chat message.
  */
 export default function () {
-  // used for versions 2.4 and earlier
-  libWrapper.register(
-    "show-secrets",
-    "CONFIG.Item.documentClass.prototype.getChatData",
-    function (wrapped, htmlOptions = {}) {
-      if (showSecrets()) htmlOptions.secrets = this.actor.isOwner;
-      return wrapped(htmlOptions);
-    },
-    "WRAPPER"
-  );
-
-  // used for versions 3.0 and later
-  libWrapper.register(
-    "show-secrets",
-    "dnd5e.dataModels.ItemDataModel.prototype.getCardData",
-    function (wrapped, enrichmentOptions = {}) {
-      if (showSecrets()) enrichmentOptions.secrets = this.parent.isOwner;
-      return wrapped(enrichmentOptions);
-    },
-    "WRAPPER"
-  );
+  if (foundry.utils.isNewerVersion(game.system.version, "2.9.9")) {
+    // used for versions 3.0 and later
+    libWrapper.register(
+      "show-secrets",
+      "dnd5e.dataModels.ItemDataModel.prototype.getCardData",
+      function (wrapped, enrichmentOptions = {}) {
+        if (showSecrets()) enrichmentOptions.secrets = this.parent.isOwner;
+        return wrapped(enrichmentOptions);
+      },
+      "WRAPPER"
+    );
+  } else {
+    // used for versions 2.4 and earlier
+    libWrapper.register(
+      "show-secrets",
+      "CONFIG.Item.documentClass.prototype.getChatData",
+      function (wrapped, htmlOptions = {}) {
+        if (showSecrets()) htmlOptions.secrets = this.actor.isOwner;
+        return wrapped(htmlOptions);
+      },
+      "WRAPPER"
+    );
+  }
 }
